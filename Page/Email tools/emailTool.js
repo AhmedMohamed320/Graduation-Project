23f9d84643c05db17c5c2fb90f125fff493e167e
Ahmed Mohamed <95592444+AhmedMohamed320@users.noreply.github.com>
2023-04-30 16:01:14 +0200

    Add files via upload

--- Page/Email tools/emailTool.js
+++ Page/Email tools/emailTool.js
@@ -1,39 +1,61 @@
 const divAppearAllEmails = document.getElementById("all-emails");
+const divPart2=document.querySelector(".part-2");
 let emailsSaved = [
-    {
-        id:1,
-        content:`
-        Welcome to [Product/Service Name]! 
-        Dear [Name], 
+    // {
+    //     id:1,
+    //     content:`
+    //     Welcome to [Product/Service Name]! 
+    //     Dear [Name], 
     
-        We are excited to welcome you as a new customer of [Product/Service Name]! We believe that our [Product/Service] will be an essential part of your journey ahead. 
+    //     We are excited to welcome you as a new customer of [Product/Service Name]! We believe that our [Product/Service] will be an essential part of your journey ahead. 
     
-        As a courtesy, [add any details about any special offers]. To learn more about [Product/Service Name], we invite you to explore our website and feel free to contact our team of experts with any questions that may arise. 
+    //     As a courtesy, [add any details about any special offers]. To learn more about [Product/Service Name], we invite you to explore our website and feel free to contact our team of experts with any questions that may arise. 
     
-        We hope your experience with us is both enjoyable and beneficial!
+    //     We hope your experience with us is both enjoyable and beneficial!
     
-        Warm regards, 
-        [Name/Company] Team
-        `
-    },
-    {
-        id:2,
-        content:`
-        Welcome to [Product/Service Name]! 
-        Dear [Name], 
+    //     Warm regards, 
+    //     [Name/Company] Team
+    //     `
+    // },
+    // {
+    //     id:2,
+    //     content:`
+    //     Welcome to [Product/Service Name]! 
+    //     Dear [Name], 
     
-        We are excited to welcome you as a new customer of [Product/Service Name]! We believe that our [Product/Service] will be an essential part of your journey ahead. 
+    //     We are excited to welcome you as a new customer of [Product/Service Name]! We believe that our [Product/Service] will be an essential part of your journey ahead. 
     
-        As a courtesy, [add any details about any special offers]. To learn more about [Product/Service Name], we invite you to explore our website and feel free to contact our team of experts with any questions that may arise. 
+    //     As a courtesy, [add any details about any special offers]. To learn more about [Product/Service Name], we invite you to explore our website and feel free to contact our team of experts with any questions that may arise. 
     
-        We hope your experience with us is both enjoyable and beneficial!
+    //     We hope your experience with us is both enjoyable and beneficial!
     
-        Warm regards, 
-        [Name/Company] Team
-        `
-    }
+    //     Warm regards, 
+    //     [Name/Company] Team
+    //     `
+    // },
+    // {
+    //     id:2,
+    //     content:`
+    //     Welcome to [Product/Service Name]! 
+    //     Dear [Name], 
+    
+    //     We are excited to welcome you as a new customer of [Product/Service Name]! We believe that our [Product/Service] will be an essential part of your journey ahead. 
+    
+    //     As a courtesy, [add any details about any special offers]. To learn more about [Product/Service Name], we invite you to explore our website and feel free to contact our team of experts with any questions that may arise. 
+    
+    //     We hope your experience with us is both enjoyable and beneficial!
+    
+    //     Warm regards, 
+    //     [Name/Company] Team
+    //     `
+    // }
 ];
-
+if(emailsSaved.length==0){
+    divAppearAllEmails.classList.add("hide");
+    divPart2.classList.add("onePage");
+}else{
+    divAppearAllEmails.classList.remove("hide");
+    divPart2.classList.remove("onePage");
 emailsSaved.forEach(element => {
     let emailContent=element.content;
     let divEmail=`
@@ -54,3 +76,4 @@ emailsSaved.forEach(element => {
         alert("Text copied to clipboard!");
     });
 });
+}
