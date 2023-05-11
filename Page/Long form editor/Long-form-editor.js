a02b9fe8c768c742903de0afc2d999d484f9a1fa
Ahmed Mohamed <95592444+AhmedMohamed320@users.noreply.github.com>
2023-05-12 02:51:08 +0300

    Add files via upload

--- Page/Long form editor/Long-form-editor.js
+++ Page/Long form editor/Long-form-editor.js
@@ -1,105 +1,91 @@
-let optionsButtons = document.querySelectorAll(".option-button");
-let advancedOptionButton = document.querySelectorAll(".adv-option-button");
-let fontName = document.getElementById("fontName");
-let writingArea = document.getElementById("text-input");
-let linkButton = document.getElementById("createLink");
-let alignButtons = document.querySelectorAll(".align");
-let spacingButtons = document.querySelectorAll(".spacing");
-let formatButtons = document.querySelectorAll(".format");
-let scriptButtons = document.querySelectorAll(".script");
-
-//List of fontlist
-let fontList = [
-    "Arial",
-    "Verdana",
-    "Times New Roman",
-    "Garamond",
-    "Georgia",
-    "Courier New",
-    "cursive",
-];
-
-//Initial Settings
-const initializer = () => {
-    //function calls for highlighting buttons
-    //No highlights for link, unlink,lists, undo,redo since they are one time operations
-    highlighter(alignButtons, true);
-    highlighter(spacingButtons, true);
-    highlighter(formatButtons, false);
-    highlighter(scriptButtons, true);
-
-    //create options for font names
-    fontList.map((value) => {
-        let option = document.createElement("option");
-        option.value = value;
-        option.innerHTML = value;
-        fontName.appendChild(option);
-    });
-};
-
-//main logic
-const modifyText = (command, defaultUi, value) => {
-    //execCommand executes command on selected text
-    document.execCommand(command, defaultUi, value);
-};
-
-//For basic operations which don't need value parameter
-optionsButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        modifyText(button.id, false, null);
-    });
-});
-
-//options that require value parameter (e.g colors, fonts)
-advancedOptionButton.forEach((button) => {
-    button.addEventListener("change", () => {
-        modifyText(button.id, false, button.value);
-    });
-});
-
-//link
-linkButton.addEventListener("click", () => {
-    let userLink = prompt("Enter a URL");
-    //if link has http then pass directly else add https
-    if (/http/i.test(userLink)) {
-        modifyText(linkButton.id, false, userLink);
+function formatDoc(cmd, value = null) {
+    if (value) {
+        document.execCommand(cmd, false, value);
     } else {
-        userLink = "http://" + userLink;
-        modifyText(linkButton.id, false, userLink);
+        document.execCommand(cmd);
+    }
+}
+
+function addLink() {
+    const url = prompt("Insert url");
+    formatDoc("createLink", url);
+}
+
+const content = document.getElementById("content");
+
+content.addEventListener("mouseenter", function () {
+    const a = content.querySelectorAll("a");
+    a.forEach((item) => {
+        item.addEventListener("mouseenter", function () {
+            content.setAttribute("contenteditable", false);
+            item.target = "_blank";
+        });
+        item.addEventListener("mouseleave", function () {
+            content.setAttribute("contenteditable", true);
+        });
+    });
+});
+
+const filename = document.getElementById("filename");
+
+function fileHandle(value) {
+    if (value === "new") {
+        content.innerHTML = "";
+        filename.value = "untitled";
+    } else if (value === "txt") {
+        const blob = new Blob([content.innerText]);
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = `${filename.value}.txt`;
+        link.click();
+    } else if (value === "pdf") {
+        html2pdf(content).save(filename.value);
+    }
+}
+
+// -------------------------------------
+const divAskFeather=document.querySelector(".askFeather");
+const InputPressSpace=document.querySelector("#pressSpace");
+const inputDraftWithAi = document.getElementById("inputDraftWithAi");
+const selectOptions = document.querySelectorAll(".select .option");
+const divStepOne=document.querySelector(".step1");
+const divStepTwo=document.querySelector(".step2");
+const optionReload = document.querySelector(".reload");
+const optionClose = document.querySelector(".close");
+
+const headP = document.querySelector(".heading");
+selectOptions.forEach((option) => {
+    option.addEventListener("click", () => {
+        const optionValue = option.querySelector("p").textContent.trim();
+        inputDraftWithAi.value = optionValue;
+    });
+});
+
+inputDraftWithAi.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        content.innerText = "";
+        headP.textContent = inputDraftWithAi.value;
+        inputDraftWithAi.value = "Tell Al what to do next...";
+        divStepOne.style.display="none";
+        divStepTwo.style.display="flex";
+        InputPressSpace.style.display="none";
     }
 });
 
-//Highlight clicked button
-const highlighter = (className, needsRemoval) => {
-    className.forEach((button) => {
-        button.addEventListener("click", () => {
-            //needsRemoval = true means only one button should be highlight and other would be normal
-            if (needsRemoval) {
-                let alreadyActive = false;
+optionReload.addEventListener("click", ()=>{
+    location.reload();
+})
 
-                //If currently clicked button is already active
-                if (button.classList.contains("active")) {
-                    alreadyActive = true;
-                }
+optionClose.addEventListener("click", ()=>{
+    divAskFeather.style.display="none";
+})
 
-                //Remove highlight from other buttons
-                highlighterRemover(className);
-                if (!alreadyActive) {
-                    //highlight clicked button
-                    button.classList.add("active");
-                }
-            } else {
-                //if other buttons can be highlighted
-                button.classList.toggle("active");
-            }
-        });
-    });
-};
-
-const highlighterRemover = (className) => {
-    className.forEach((button) => {
-        button.classList.remove("active");
-    });
-};
-
-window.onload = initializer();
+InputPressSpace.addEventListener("keydown", function (event) {
+    if (event.code !== "Space") {
+        event.preventDefault();
+    }
+    else{
+        divAskFeather.style.display = "flex";
+    }
+});
