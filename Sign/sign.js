9b88b19ee74cfca3c2450b3d6b913ed38fa5f493
Ahmed Mohamed <95592444+AhmedMohamed320@users.noreply.github.com>
2023-05-25 21:20:35 +0300

    Delete Sign directory

--- Sign/sign.js
+++
@@ -1,9 +0,0 @@
-const btnSignWithPassword=document.getElementById("signWithPassword");
-const labelPassword = document.getElementById("password-label");
-const inputPassword = document.getElementById("password");
-
-btnSignWithPassword.addEventListener("click",(e)=>{
-    e.preventDefault();
-    labelPassword.classList.toggle("active");
-    inputPassword.classList.toggle("active");
-})
