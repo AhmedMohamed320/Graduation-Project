18632e61f3f1c3a26f615d4d5c62860574729aa0
Ahmed Mohamed <95592444+AhmedMohamed320@users.noreply.github.com>
2023-05-27 18:33:08 +0300

    Add files via upload

--- Page/AI article writer/aIArticleWriter.js
+++ Page/AI article writer/aIArticleWriter.js
@@ -11,7 +11,9 @@ inputSearchKeywords.addEventListener("input", function () {
 
 const inputAddTag = document.querySelector(".inputAddTag");
 const btnNext = document.querySelector(".complete-the-fields .btnNext");
-const divSomeKeyWords = document.querySelector(".complete-the-fields.one .someKeyWords");
+const divSomeKeyWords = document.querySelector(
+    ".complete-the-fields.one .someKeyWords"
+);
 
 inputAddTag.addEventListener("keyup", (event) => {
     if (event.keyCode === 13) {
@@ -71,8 +73,7 @@ allBtnNext.forEach((btn) => {
         if (numDiv === 1 && divSomeKeyWords.childElementCount > 0) {
             numDiv++;
             showDivStep();
-        } 
-        else if (numDiv < 4 && numDiv>1) {
+        } else if (numDiv < 4 && numDiv > 1) {
             numDiv++;
             showDivStep();
         }
@@ -89,48 +90,47 @@ allBtnBack.forEach((btn) => {
     });
 });
 
+// ------------------------
+
 const allDivOfSteps = document.querySelectorAll(".complete-the-fields");
 function showDivStep() {
     const pStepNum = document.querySelectorAll(".stepNum");
     pStepNum.forEach((stepNum) => {
-        if (stepNum.textContent <= numDiv) {
+        if (stepNum.textContent == numDiv) {
             stepNum.parentElement.classList.add("active");
+            stepNum.parentElement.classList.remove("activeLast");
+        } else if (stepNum.textContent < numDiv) {
+            stepNum.parentElement.classList.add("activeLast");
         } else {
+            stepNum.parentElement.classList.remove("activeLast");
             stepNum.parentElement.classList.remove("active");
         }
     });
+    hideAllDivOfSteps();
+    hideAllDivOfMainContent();
+    divEmpty.style.display = "flex";
+
     if (numDiv === 1) {
-        hideAllDivOfSteps();
         const activeStepOne = document.querySelector(
             ".complete-the-fields.one"
         );
         activeStepOne.style.display = "flex";
     } else if (numDiv === 2) {
-        hideAllDivOfMainContent();
-        hideAllDivOfSteps();
-        divEmpty.style.display = "flex";
         const activeStepTwo = document.querySelector(
             ".complete-the-fields.two"
         );
         activeStepTwo.style.display = "flex";
     } else if (numDiv === 3) {
-        hideAllDivOfMainContent();
-        hideAllDivOfSteps();
-        divEmpty.style.display = "flex";
         const activeStepThree = document.querySelector(
             ".complete-the-fields.three"
         );
         activeStepThree.style.display = "flex";
     } else if (numDiv === 4) {
-        hideAllDivOfMainContent();
-        hideAllDivOfSteps();
-        divEmpty.style.display = "flex";
         const activeStepFour = document.querySelector(
             ".complete-the-fields.four"
         );
         activeStepFour.style.display = "flex";
     }
-
 }
 showDivStep();
 function hideAllDivOfSteps() {
@@ -138,13 +138,13 @@ function hideAllDivOfSteps() {
         div.style.display = "none";
     });
 }
-// --------------------
 function hideAllDivOfMainContent() {
     const AllDivInPart_2 = document.querySelectorAll(".part-2-2>div");
     AllDivInPart_2.forEach((div) => {
         div.style.display = "none";
     });
 }
+// --------------------
 
 const btnGenerateIdeas = document.querySelector(".GenerateIdeas");
 btnGenerateIdeas.addEventListener("click", () => {
@@ -166,12 +166,38 @@ btnGenerateArticle.addEventListener("click", () => {
     hideAllDivOfMainContent();
     const DivEndArt = document.querySelector(".endArt");
     DivEndArt.style.display = "block";
+    const endStepLastActive = document.querySelector(".step-by-step .four");
+    endStepLastActive.classList.add("activeLast");
 });
+// ----------------------
+
+const iconDeleteKeyInIdeas = document.querySelectorAll(
+    ".complete-the-fields.two .deleteKey"
+);
+iconDeleteKeyInIdeas.forEach((item) => {
+    item.addEventListener("click", () => {
+        iconDeleteKeyInIdeas.parentElement.remove();
+    });
+});
+
+// ----------------------
+function DeleteTalkingPoint() {
+    const iconDeleteTalkingPoint = document.querySelectorAll(
+        ".complete-the-fields.four .up .oneArt .icon-remove"
+    );
+    iconDeleteTalkingPoint.forEach((item) => {
+        item.addEventListener("click", () => {
+            item.parentElement.parentElement.remove();
+        });
+    });
+}
+
+DeleteTalkingPoint();
 // ----------------------
 const addTalkingPoint = document.querySelectorAll(".addTalkingPoint");
 addTalkingPoint.forEach((inputAdd) => {
     inputAdd.addEventListener("keyup", (event) => {
-        if (event.keyCode === 13) {
+        if (event.keyCode === 13 && inputAdd.value != "") {
             const point = document.createElement("div");
             point.classList.add("oneArt");
             point.innerHTML = `
@@ -179,69 +205,119 @@ addTalkingPoint.forEach((inputAdd) => {
                 src="../../images/enable to darg and drop.svg"
                 alt=""
                 />
-                <p>${inputAdd.value}</p>
+                <div class="img">
+                    <ion-icon
+                        name="checkmark-outline"
+                    ></ion-icon>
+                </div>
+                <span>
+                    <p>${inputAdd.value}</p>
+
+                    <ion-icon
+                        name="remove-outline"
+                        class="icon-remove"
+                    ></ion-icon>
+                </span>
     `;
             inputAdd.parentNode.insertBefore(
                 point,
                 inputAdd.parentNode.lastElementChild
             );
-            point.setAttribute("draggable", true);
+            dragAndDrop();
+            DeleteTalkingPoint();
             inputAdd.value = "";
         }
     });
 });
 
-// function HeadArtOutline() {
-//     const firstDivOutline = document.querySelectorAll(
-//         ".up>.oneArt:nth-of-type(1)"
-//     );
-//     const img = document.createElement("div");
-//     img.classList.add("img");
-//     img.innerHTML = `
-//         <ion-icon
-//         name="checkmark-outline"
-//         ></ion-icon>
-//     `;
-//     firstDivOutline.forEach((div) => {
-//         div.insertBefore(img, div.firstElementChild.nextSibling);
-//     });
-// }
-
-// HeadArtOutline();
-
-var allCols = document.querySelectorAll(".Article-Outline .up");
-var items = document.querySelectorAll(".oneArt");
-items.forEach(function (element) {
-    element.setAttribute("draggable", "true");
-});
-
-let drag = null;
-items.forEach((column) => {
-    column.addEventListener("dragstart", () => {
-        drag = column;
-        column.style.opacity = 0.5;
+function dragAndDrop() {
+    const allCols = document.querySelectorAll(".Article-Outline .up");
+    let items = document.querySelectorAll(".oneArt");
+    items.forEach(function (element) {
+        element.draggable = true;
     });
 
-    column.addEventListener("dragend", () => {
-        drag = null;
-        column.style.opacity = 1;
-    });
+    let drag = null;
+    items.forEach((column) => {
+        column.addEventListener("dragstart", (e) => {
+            e.dataTransfer.setData("text/plain", e.target.id);
+            drag = column;
+            column.style.opacity = 0.5;
+        });
 
-    allCols.forEach((col) => {
-        col.addEventListener("dragover", function (e) {
-            if (drag && !drag.classList.contains("up")) {
-                e.preventDefault();
-            }
+        column.addEventListener("dragend", () => {
+            drag = null;
+            column.style.opacity = 1;
         });
-        col.addEventListener("dragleave", function () {
-            this.style.background = "none";
-        });
-        col.addEventListener("drop", function (e) {
-            if (drag && !drag.classList.contains("up")) {
-                e.preventDefault();
-                this.insertBefore(drag, this.children[0]);
+
+        allCols.forEach((col) => {
+            col.addEventListener("dragover", function (e) {
+                if (
+                    drag &&
+                    !drag.classList.contains("up") &&
+                    this.classList.contains(drag.closest(".up").classList)
+                ) {
+                    e.preventDefault();
+                }
+            });
+            col.addEventListener("dragleave", function () {
                 this.style.background = "none";
-            }
+            });
+            col.addEventListener("drop", function (e) {
+                if (
+                    drag &&
+                    !drag.classList.contains("up") &&
+                    this.classList.contains(drag.closest(".up").classList)
+                ) {
+                    e.preventDefault();
+                    this.insertBefore(drag, e.target);
+                    this.style.background = "none";
+                }
+            });
         });
     });
+}
+dragAndDrop();
+// -------------------
+
+const divSelectedTone = document.querySelector(".divSelect.tone");
+const divSelectedLanguage = document.querySelector(".divSelect.language");
+const ulSelectTone = document.querySelector(".ulSelect.tone");
+const ulSelectLanguage = document.querySelector(".ulSelect.language");
+const liSelectLanguage = document.querySelectorAll(".ulSelect.language ul li");
+const liSelectTone = document.querySelectorAll(".ulSelect.tone ul li");
+const valueSelectLanguage = document.querySelector(
+    ".divSelect.language .valueSelect"
+);
+const valueSelectTone = document.querySelector(".divSelect.tone .valueSelect");
+
+divSelectedLanguage.addEventListener("click", () => {
+    divSelectedLanguage.classList.toggle("active");
+    ulSelectLanguage.classList.toggle("active");
 });
+
+divSelectedTone.addEventListener("click", () => {
+    divSelectedTone.classList.toggle("active");
+    ulSelectTone.classList.toggle("active");
+});
+
+liSelectLanguage.forEach((li) => {
+    li.addEventListener("click", () => {
+        valueSelectLanguage.innerHTML = li.innerHTML;
+        ulSelectLanguage.classList.remove("active");
+    });
+});
+
+liSelectTone.forEach((li) => {
+    li.addEventListener("click", () => {
+        valueSelectTone.innerHTML = li.innerHTML;
+        ulSelectTone.classList.remove("active");
+    });
+});
+
+// -----------------------
+const iconExport=document.querySelector(".iconExport");
+const divOptionExport=document.querySelector(".optionExport");
+iconExport.addEventListener("click",()=>{
+    divOptionExport.classList.toggle("active");
+})
